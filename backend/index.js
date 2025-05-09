cef70744e9e43bd0e7bf5350451bf44fb19dd24f
KaiVerleysenPXL <kai.verleysen@student.pxl.be>
2025-05-09 17:22:28 +0200

    fix: saving passwords in bcrypt over plain-text

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 
 const port=3000;
 
@@ -31,15 +32,19 @@ app.get('/authenticate/:username/:password', async (request, response) => {
     const password = request.params.password;
 
     // const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
-    const query = 'SELECT * FROM users WHERE user_name = $1 AND password = $2'
+    const query = 'SELECT * FROM users WHERE user_name = $1'
 
     console.log(query);
     // pool.query(query, (error, results) => {
-    pool.query(query, [username, password], (error, results) => {
+    pool.query(query, [username], async (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).json(results.rows)});
+      // response.status(200).json(results.rows)});
+      const passwordIsValid = await bcrypt.compare(password, results.rows[0].password)
+      if (passwordIsValid) {
+        response.status(200).json(results.rows);
+      }});
 });
 
 app.listen(port, () => {
--- db/database.sql
+++ db/database.sql
@@ -8,7 +8,7 @@ BEGIN;
 create table users (id serial primary key, user_name text not null unique, password text not null);
 grant all privileges on table users to secadv;
 
-insert into users (user_name, password) values ('pxl-admin', 'insecureandlovinit') ;
-insert into users (user_name, password) values ('george', 'iwishihadbetteradmins') ;
+insert into users (user_name, password) values ('pxl-admin', '$2a$12$0dcGPT5RTTKtUJbpvu/E/uUpV19gW3UPYK1moV7eApa7GCDUoLJCG') ;
+insert into users (user_name, password) values ('george', '$2a$12$SWYcOpaW66XPWoR1Rw746eHIsDuRtYlu1i40MqZaXZNaZpCKSbYU6') ;
 
 COMMIT;
